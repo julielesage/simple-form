214eb9b37156f97fa55ef50a28f15836fca28d29
julielesage <lesage.julie@gmail.com>
2020-04-04 12:12:43 +0200

    centré

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Footer from "./components/Footer";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //ouais ce sont des crochets !!!!!
+  const [step, setStep] = useState(0);
+  const [userName, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordbis, setPasswordbis] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {/* //scope pour pas que le console.log ne soit pas pris pour du texte */}
+      {console.log(step)}
+      <p style={{ color: "red" }}>{errorMessage}</p>
+      {step === 0 ? (
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            if (
+              userName === "" ||
+              email === "" ||
+              password === "" ||
+              passwordbis === ""
+            ) {
+              setErrorMessage("Please fill the entire form before submitting");
+            } else if (password !== passwordbis) {
+              setErrorMessage("your password has not been confirmed");
+            } else setStep(1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1>Create Account</h1>
+          <main>
+            <p>Name</p>
+            <input
+              type="text"
+              value={userName}
+              placeholder="type your firstname and lastname here"
+              onChange={event => {
+                setUsername(event.target.value);
+              }}
+            ></input>
+            <p>Email</p>
+            <input
+              type="email"
+              value={email}
+              placeholder="type your email here"
+              onChange={event => {
+                setEmail(event.target.value);
+              }}
+            ></input>
+            <p>Password</p>
+            <input
+              type="password"
+              value={password}
+              placeholder="choose a password here"
+              onChange={event => {
+                setPassword(event.target.value);
+              }}
+            ></input>
+            <p>Confirm your password</p>
+            <input
+              type="password"
+              value={passwordbis}
+              placeholder="confirm by typing again your password"
+              onChange={event => {
+                setPasswordbis(event.target.value);
+              }}
+            ></input>
+          </main>
+          <button type="submit">Register</button>
+        </form>
+      ) : (
+        <div>
+          <h2>Results</h2>
+          <p>Name : {userName}</p>
+          <p>Email : {email}</p>
+          <p>Password : {password}</p>
+          <button
+            onClick={event => {
+              setStep(0);
+            }}
+          >
+            Modify your information
+          </button>
+        </div>
+      )}
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import "style.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        Made by{" "}
+        <a href="https://www.linkedin.com/in/julie-lesage-92a09117/">
+          Julie Lesage
+        </a>
+        &nbsp; at Le Reacteur, Paris.
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
